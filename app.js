66bd4a65b8c07e4e38f2aa9936cb791dd3c6a83a
stuckincallback <stuckincallback@gmail.com>
2019-04-05 02:07:00 +0530

    1. .dockerignore 2.Docker File 3.package.json

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 3000;
 app.get('/', (req, res)=>  {
     res.send('hello world');
 })
 
-app.listen(3000);
+app.listen(port);
